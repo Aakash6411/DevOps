b154287efe92c9c4955393ae03d14cd0ae352974
Aakash6411 <as2042896@gmail.com>
2026-04-21 14:10:31 +0530

    DevOps files

--- network.tf
+++ network.tf
@@ -48,6 +48,18 @@ resource "azurerm_network_security_group" "jenkins_SG" {
     destination_address_prefix = "*"
   }
 
+   security_rule {
+    name                       = "jenkinsport"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "Production"
   }
