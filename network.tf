a0a79ebd2fcb7ef2539e8bf65821f1ec628dfc79
Aakash6411 <as2042896@gmail.com>
2026-04-21 14:11:23 +0530

    DevOps files

--- network.tf
+++ network.tf
@@ -60,6 +60,18 @@ resource "azurerm_network_security_group" "jenkins_SG" {
     destination_address_prefix = "*"
   }
 
+   security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = {
     environment = "Production"
   }
