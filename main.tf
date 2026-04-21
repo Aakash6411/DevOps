142792964671dfa545163e867c3543332fee5ddc
Aakash6411 <as2042896@gmail.com>
2026-04-21 12:43:32 +0530

    DevOps files

--- main.tf
+++ main.tf
@@ -6,3 +6,16 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "RG" {
+  name = local.resource_group_name
+  location = local.location
+}
